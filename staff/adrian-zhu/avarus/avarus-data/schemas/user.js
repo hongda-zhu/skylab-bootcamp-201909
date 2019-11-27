dd8b0f4b74e815768e15ed86bce82b176720b5bb
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-27 11:53:44 +0100

    upgrade

--- staff/adrian-zhu/avarus/avarus-data/schemas/user.js
+++ staff/adrian-zhu/avarus/avarus-data/schemas/user.js
@@ -13,7 +13,8 @@ module.exports = new Schema({
     },
     email: {
         type: String,
-        required:true
+        required:true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     password: {
         type: String,
